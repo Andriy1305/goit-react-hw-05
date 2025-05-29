af03eec3f79a478b5eb84c2cd588e6ed3e34bea2
Andriy Ivanchenko <mais-nasinya@ukr.net>
2025-05-29 10:36:38 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchTrendingMovies } from "../../services/tmdbApi";
-import { Link } from "react-router-dom";
+import MovieList from "../../components/MovieList/MovieList";
 import styles from "./HomePage.module.css";
 
 const HomePage = () => {
@@ -17,15 +17,7 @@ const HomePage = () => {
     <main className={styles.main}>
       <h1 className={styles.tittle}>Trending Movies</h1>
       {error && <p>{error}</p>}
-      <ul className={styles.list}>
-        {movies.map(({ id, title }) => (
-          <li key={id} className={styles.item}>
-            <Link to={`/movies/${id}`} className={styles.link}>
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <MovieList movies={movies} />
     </main>
   );
 };
